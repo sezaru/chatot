35cb464d0855471620603b0d2ccf87a20553431d
Eduardo Barreto Alexandre <279828+sezaru@users.noreply.github.com>
2026-09-09 12:48:02 -0300

    chore: release 0.7.1-beta

--- .nix/package.nix
+++ .nix/package.nix
@@ -46,7 +46,7 @@
         ${webp-pixbuf-loader}/${gdk-pixbuf.moduleDir}/*.so \
         > $out
     '';
-  version = "0.7.0-beta";
+  version = "0.7.1-beta";
 in
   buildGoModule {
     pname = "chatot";
